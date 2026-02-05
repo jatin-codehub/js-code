e659eadbd762d32b6764487fd79ee44e140f1d1c
jatin <jatinprajapati0115@gmail.com>
2026-02-05 12:51:18 +0000

    comparsion

---
+++ basics/04_comparsion.js
@@ -0,0 +1,13 @@
+// console.log(2>1); // => true (boolean)
+// console.log(2<=1); //  => false
+// /(2==1),(2!==1),
+
+console.log("2">1);
+
+console.log(null>0); // => false // // in comp null as undefined
+console.log(null==0); // => false
+console.log(null>=0); // => true // // null as number
+// // rplace null to undefined => false
+// === use to compare stritly including datatype
+
+/* ------use clean code ------------*/
