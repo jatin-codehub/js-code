30d99801994b58c045272ea5b8e51ac0818f0003
jatin <jatinprajapati0115@gmail.com>
2026-02-05 10:50:13 +0000

    datatypes

---
+++ basics/variable.js
@@ -0,0 +1,10 @@
+const accountid=67678
+let accemail="jatin@gmail.com"
+var accpassward="011234"
+acccity="jansath"
+// to define value
+console.log(accountid);
+accemail="jatin3mail.com"
+acccity="muz"
+ let accstate;
+console.table([accemail,accountid,accpassward,acccity,accstate]);
