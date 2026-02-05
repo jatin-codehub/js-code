ef03568ce18b8e51c7e1a887cc3b2e116e90712a
jatin <jatinprajapati0115@gmail.com>
2026-02-05 10:49:35 +0000

    datatype

---
+++ basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as new version
+
+// alert(3+3) //we use it by diferent syntax can be done in brower
+
+console.log(3+3)
+// code readable easy
+console.log("jatin")
+
+let name ="jatin"
+let age=20
+let islogged =false
+
+// number =>2 to power 53
+// bigint //use when num limit lake
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => when value is not asign
+// symbol => unique
+
+
+// obejct 
+
+console.log(typeof null)
+
+// typeof null=> object
+// typeof undefined => undefined
