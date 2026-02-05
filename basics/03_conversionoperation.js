178c6bdaf6b8eb1d11ffce3ca32e966272088907
jatin <jatinprajapati0115@gmail.com>
2026-02-05 12:32:27 +0000

    operation

--- basics/03_conversionoperation.js
+++ basics/03_conversionoperation.js
@@ -5,7 +5,7 @@ console.log(typeof score);
 let valueInNumber=Number(score)
 console.log(typeof valueInNumber);
 
-// use non number value
+// //use non number value
 let s="33abc"
 
 console.log(typeof s);
@@ -13,17 +13,17 @@ console.log(typeof s);
 let a=Number(s)
 console.log(typeof a);
 console.log(a);
-// "33"=> 33
-// "33abc" => nan
-// give NaN means not a number
-// is Null in score then it convert into num give 0
-// same for string,boolean
+// //"33"=> 33
+// //"33abc" => nan
+// //give NaN means not a number
+// //is Null in score then it convert into num give 0
+// //same for string,boolean
 
-let b="" //blank string
+let b="" // //blank string
 let booleanisa = Boolean(b)
 console.log(booleanisa);
-// "" =>  false
-// if b ="jatin" => True
+// // "" =>  false
+// // if b ="jatin" => True
  
 let c =33
  
@@ -31,7 +31,36 @@ let stringisc =String(c)
 
 console.log(stringisc);
 console.log(typeof stringisc);
-// 33 => 33 really(  " 33"  )
-// number => string
+// //33 => 33 really(  " 33"  )
+// //number => string
+
+
+/* ------------opeation-----------*/
+
+let value = 4
+let negvalue = -value
+//console.log(negvalue);
+
+// // console.log(2+2);
+// //same for +-*/
+// // for power 2**2 // reminder %
+
+let str1 = "mr."
+let str2 = "jatin"
+let str3 = str1+ str2
+console.log(str3);// => mr.jatin
+
+// console.log("1"+2);// => 12
+// console.log(1+"2");// => 12
+// console.log("1"+2+2);//=> 122   
+// console.log(typeof ("1"+2+2)); // => string
+// // imp 
+// console.log(1+2+"2"); // => 32 -------different output 
+// console.log(typeof (1+2+"2")); // => string
+
+
+console.log(+true);  // => 1
+//console.log(true+);  //=> error-------diff
+console.log(+"");  // => 0
 
 
