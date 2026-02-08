7d8374b2c1341ee2f8b0986713070cb2ec440f03
jatin <jatinprajapati0115@gmail.com>
2026-02-08 10:46:15 +0000

    maths

--- basics/06_numandmaths.js
+++ basics/06_numandmaths.js
@@ -18,6 +18,18 @@ const b = 1000000
 // console.log(b.toLocaleString('en-us'));// => 1,000,000
 
 //----------maths-------------------
-console.log(maths);
 
-console.log(maths.abs(-4));
+console.log(Math);
+// console.log(Math.abs(-4)); // absolute change to + // => 4
+// console.log(Math.round(4.6));// => 5
+// console.log(Math.ceil(4.2));// ceil if small like 4.1 so =>5
+// console.log(Math.floor(4.3));// floor base value => 4
+// console.log(Math.min(1,8,9,6)); // => 1 for min same fr max
+
+console.log(Math.random());// limits(0 to 1)
+console.log(Math.floor(Math.random()*10 + 1));
+
+const min = 10
+const max = 20
+console.log(Math.random() * (max - min +1)+min);// => 17.26**  means to incerse range by min and max
+console.log(Math.floor(Math.random() * (max - min +1)));
